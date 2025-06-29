37163f07a7504479e49b2c463c292eae131296cf
Jesus Rodriguez Echenique <j.eh.u14@hotmail.com>
2025-06-29 12:49:39 -0500

    Insertar_actualizar_usuario.php

---
+++ Assets/SCRIPTS/EnviarPuntaje.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class EnviarPuntaje : MonoBehaviour
+{
+    public string nombreUsuario = "Ana";
+    public int puntaje = 350;
+
+    void Start()
+    {
+        StartCoroutine(EnviarDatos());
+    }
+
+    IEnumerator EnviarDatos()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("nombre_usuario", nombreUsuario);
+        form.AddField("puntaje", puntaje);
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/juego4/insertar_actualizar_usuario.php", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.Log("Error: " + www.error);
+            }
+            else
+            {
+                Debug.Log("Respuesta del servidor: " + www.downloadHandler.text);
+            }
+        }
+    }
+}
