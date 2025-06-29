a858cf670aa6ce2e76cd81ed200e0f6b1daf8df2
Jesus Rodriguez Echenique <j.eh.u14@hotmail.com>
2025-06-29 14:50:36 -0500

    Obtener_Ranking.php

---
+++ Assets/SCRIPTS/ObtenerRanking.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class ObtenerRanking : MonoBehaviour
+{
+    public Text rankingTexto;
+
+    void Start()
+    {
+        StartCoroutine(ConsultarRanking());
+    }
+
+    IEnumerator ConsultarRanking()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost/juego4/obtener_ranking.php"))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.Log("Error: " + www.error);
+            }
+            else
+            {
+                rankingTexto.text = www.downloadHandler.text;
+            }
+        }
+    }
+}
